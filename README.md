c49c7a1b7e516662da862ad2e1cf311947ec0475
Jamie Atkinson <jamie@atkinson.io>
2017-11-24 09:59:44 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# matcher-backend
+# FlaskyMcFlaskFace
+
+Demo flask app to demonstrate how to access a remote database and upload and handle user files.
---
+++ app.py
@@ -0,0 +1,85 @@
+"""
+Simple flask app
+"""
+import os
+from flask import (Flask, request, redirect, url_for, send_from_directory,
+                   flash, render_template)
+import pandas as pd
+from sqlalchemy import create_engine
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+# Set the location where uploaded files will be stored (/tmp/ is probably ok)
+# Note that at present these will remain on the server until it is rebooted.
+
+UPLOAD_FOLDER = '/tmp/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Set the app secret key to prevent CSRF
+
+app.secret_key = os.urandom(24)
+
+# Which extensions are allowed to be uploaded?
+
+ALLOWED_EXTENSIONS = set(['txt', 'csv', 'json'])
+
+# Demonstrate access to an attached database using sqlalchemy and pandas
+
+@app.route('/', methods=['GET'])
+def main_page():
+    return "hello"
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+def allowed_file(filename):
+    """
+    Check that the selected file is allowed
+
+    :param filename: <string> Filename of local file to be uploaded
+    :return: <boolean> True if filename is valid and extension is in
+     ALLOWED_EXTENSIONS, else False.
+    """
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Demonstrate an example of how to upload files to the server.
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    """
+    Upload a file using a simple form
+    """
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash(u'No file part', 'error')
+            return redirect(request.url)
+        selected_file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if selected_file.filename == '':
+            flash(u'No selected file', 'error')
+            return redirect(request.url)
+        if selected_file and not allowed_file(selected_file.filename):
+            flash(u'File is not of authorised type', 'error')
+            return redirect(request.url)
+        if selected_file and allowed_file(selected_file.filename):
+            filename = secure_filename(selected_file.filename)
+            selected_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template('upload.html')
+
+# Render the file back to the user.
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    """
+    Render uploaded file as a new webpage
+    """
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
